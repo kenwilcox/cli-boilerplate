25ad29c79d85d30264b87409cf8e86093e68055d
Kenneth Wilcox <wilcoxk@gmail.com>
2014-09-05 15:54:06 -0600

    Ugh - looks like windows doesn't like the double quotes

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
 
 var fs   = require('fs')
 var os   = require('os');
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
 
-fs.readFile('boilerplate.html', function read(err, data) {
+//process.chdir(__dirname.split("/").pop());
+//console.log(__dirname.split("/").pop() + '/boilerplate.html');
+// Make sure we open the right file
+fs.readFile(__dirname.split("/").pop() + '/boilerplate.html', function read(err, data) {
   if (err) return console.log(err)
   var command = "echo '" + data.toString() + "' | ";
   var platform = os.platform();
   if (platform === "darwin") {
-    command += "pbcopy";
-  } else if (platform === "win64" || platform === "win32" {
-    command += "clip";
+    command = "echo '" + data.toString() + "' | pbcopy";
+  } else if (platform === "win64" || platform === "win32") {
+    command = 'echo "' + data.toString().replace("\"", "'") + '" | clip';
   }
   // TODO - figure out which command to use for platform === 'linux'
+  console.log(command);
   exec(command, function clipboarded(err, stdout, stdrr) {
     if (err) return console.log(err)
     console.log("Copied!")
